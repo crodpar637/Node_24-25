6049a16e5ed45da302bf419edd72469e8ca32952
crodpar637 <crodpar637@ieshnosmachado.org>
2025-02-21 00:08:17 +0100

    Auto save changes

--- backend_bar/config/config.js
+++ backend_bar/config/config.js
@@ -3,13 +3,13 @@ require('dotenv').config({
 });
 
 module.exports = {
-  port: process.env.PORT || 80,
+  port: process.env.PORT || 3000,
   db: {
-    host: process.env.DB_HOST || "crossover.proxy.rlwy.net",
+    host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "cZMTrkdvLIIkOpSbbUPvpuQNqCTAmtpu",
+    password: process.env.DB_PASSWORD || "test",
     name: process.env.DB_NAME || "bar",
-    port: process.env.DB_PORT || 16302,
+    port: process.env.DB_PORT || 3306,
   },
   secretKey: process.env.SECRET_KEY || "default_secret",
 };
--- backend_bar/sql/bar.sql
+++ backend_bar/sql/bar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 09-01-2025 a las 12:43:43
+-- Tiempo de generación: 20-02-2025 a las 16:46:23
 -- Versión del servidor: 8.0.39
 -- Versión de PHP: 8.2.8
 
@@ -41,7 +41,6 @@ CREATE TABLE `pedidos` (
 INSERT INTO `pedidos` (`idpedido`, `cliente`, `idplato`, `unidades`) VALUES
 (1, 'Paco', 40, 2),
 (2, 'Juan', 36, 2),
-(3, 'Susana', 34, 3),
 (4, 'Paco', 37, 1),
 (5, 'Juan', 38, 2),
 (6, 'Susana', 35, 1),
@@ -50,8 +49,6 @@ INSERT INTO `pedidos` (`idpedido`, `cliente`, `idplato`, `unidades`) VALUES
 (9, 'Gloria', 43, 3),
 (10, 'Gloria', 44, 2),
 (11, 'Juan', 38, 2),
-(12, 'Angel', 34, 1),
-(13, 'Sergio', 34, 4),
 (14, 'Hugo', 43, 5),
 (15, 'HUGO', 44, 1),
 (16, 'HUGO', 39, 5),
@@ -75,18 +72,51 @@ CREATE TABLE `platos` (
 --
 
 INSERT INTO `platos` (`idplato`, `nombre`, `descripcion`, `precio`) VALUES
-(34, 'SERRANITO', 'Bocadillo de origen sevillano.', 5.50),
-(35, 'JAMÓN IBÉRICO 7 JOTAS ', 'Jamón ibérico de bellota de calidad excepcional 7 bellotas.', 20.00),
+(35, 'Jamón ibérico 7 jotas', 'Jamón ibérico de bellota de calidad excepcional 7 bellotas.', 32.00),
 (36, 'GAMBAS', 'GAMBAS BLANCAS DE HUELVA COCIDAS EN AGUA DE MAR', 10.00),
 (37, 'PATATAS BRAVAS', 'Patatas cortadas en forma cúbica con salsa brava', 3.50),
 (38, 'SOLOMILLO AL WHISKEY', 'Solomillo de cerdo ibérico en la salsa sevillana al whiskey', 3.00),
 (39, 'GARBANZOS CON ESPINACAS', 'Guiso sevillano de garbanzos con espinacas', 2.50),
-(40, 'BACALAO CON TOMATE', 'Clásico sevillano para la época de cuaresma, el famoso bacalao con tomate', 4.00),
+(40, 'BACALAO CON TOMATE', 'Clásico sevillano para la época de cuaresma, el famoso bacalao con tomate casero', 5.25),
 (41, 'MONTADITO DE PRINGÁ', 'Auténtico montadito sevillano de pringá con chorizo, morcilla y carne de cerdo y ternera.', 2.75),
 (42, 'CARACOLES', 'Caracoles, un plato no apto para guiris.', 2.50),
 (43, 'CABRILLAS EN TOMATE', 'Cabrillas en salsa de tomate, como los caracoles, pero más grandes.', 3.50),
 (44, 'PAVÍAS DE BACALAO', 'Como decía El Pali monstrandosu espíritu pacifista y afición a este plato \"Menos misiles y más pavías de bacalao\" ', 4.50),
-(45, 'PAPAS ALIÑÁS', 'Patatas cultivadas en terrenos arenosos de Sanlúcar de Barrameda, cocidas y aliñadas con aceite de oliva virgen extra de Andalucía.', 2.50);
+(45, 'PAPAS ALIÑÁS', 'Patatas cultivadas en terrenos arenosos de Sanlúcar de Barrameda, cocidas y aliñadas con aceite de oliva virgen extra de Andalucía.', 3.00),
+(58, 'Tostada de zurrapa', 'Medio mollete untado con exquisita zurrapa blanca', 1.50),
+(61, 'Tostada', 'Pan tostado con aceite de oliva', 1.50);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `role` varchar(15) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
+(1, 'pepe', 'pepe@gmail.com', '$2b$10$WizWab3EW/O64A8g4HrisempX.zwLwLev7Uq45Z1frPD9ZbnIiQRi', 'user'),
+(2, 'juan', 'juan@gmail.com', '$2b$10$9IxtZvqFW2ikknrLaBCAo.wW45YyiPKyB1DXczZ8hmPcAXk/XSkdO', 'user'),
+(3, 'manuel', 'manuel@gmail.com', '$2b$10$/J01Z2Z4eSnymMOk7fnlpuUgnAFJbI9Fr9sP/hE4gjptomQwTw/yy', 'admin'),
+(4, 'adrian', 'adrian@gmail.com', '$2b$10$5zDll.AL51JIH.VvilmdduZWJgINx.xV7bpa.8xzlDLUbkLfoVaUC', 'user'),
+(5, 'virginia', 'virginia@gmail.com', '$2b$10$YQNU3YkFIgQwhyd7uk6p3OB.l4Z5XMwi99Dwws2K/YVcBdnDigmJ6', 'user'),
+(6, 'lucia', 'lucia@gmail.com', '$2b$10$inqNVTYYUSmQdH/kLqnQpOV22B8aB6QDerrAIE7IMRW27dzHNChhK', 'user'),
+(7, 'Santiago', 'santi@gmail.com', '$2b$10$tZ0E1SefkpudwH2r7LBEu.1bGylgsH69fvVgmnirg2guJc6v8w4py', 'user'),
+(8, 'Ángel', 'angel@gmail.com', '$2b$10$p72Y1TyCn3m2xlGBLI5PAuhqRHu5v.keuF86uKt27fvMzxz6SFHf.', 'user'),
+(9, 'Nicolai', 'nicolai@gmail.com', '$2b$10$1I66Y1dnYpaPtiqBlk2lpeQH.GRxtbfexrtax2rmNAuk8tLTuTlhq', 'user'),
+(10, 'Ángel', 'angel9144039@gmail.com', '$2b$10$mFM8nHpT72cMvwojbsib7u/ZtQRjZSO9NaOZqu5qvvJ4O/aN0nham', 'user'),
+(11, 'Ángel', 'angel4434320@gmail.com', '$2b$10$GqjmzW3BvXwO07B5qMeY0ePGMkeT/.OmXtBOMUgRZC.XQXYiLDqCK', 'user'),
+(12, 'Ángel', 'angel3084195@gmail.com', '$2b$10$lZUMweMx2p0yZoWFuP9Lu.CdQuq5wsFPloRpIGESoAna3jyLUtIli', 'user');
 
 --
 -- Índices para tablas volcadas
@@ -106,6 +136,13 @@ ALTER TABLE `platos`
   ADD PRIMARY KEY (`idplato`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -119,7 +156,13 @@ ALTER TABLE `pedidos`
 -- AUTO_INCREMENT de la tabla `platos`
 --
 ALTER TABLE `platos`
-  MODIFY `idplato` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `idplato` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
@@ -129,7 +172,7 @@ ALTER TABLE `platos`
 -- Filtros para la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  ADD CONSTRAINT `FK_PLATOS` FOREIGN KEY (`idplato`) REFERENCES `platos` (`idplato`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `FK_PLATOS` FOREIGN KEY (`idplato`) REFERENCES `platos` (`idplato`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
