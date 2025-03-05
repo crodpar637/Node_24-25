74525646fc3fe8a39934f50436c148d1131d71e1
crodpar637 <crodpar637@ieshnosmachado.org>
2025-03-05 01:06:26 +0100

    Auto save changes

--- backend_bar/index.js
+++ backend_bar/index.js
@@ -30,8 +30,8 @@ if (process.env.NODE_ENV !== "production") {
   // Configurar CORS para admitir el origen del frontend en desarrollo
   app.use(
     cors({
-      origin: "http://localhost:5173", // Permitir el frontend en desarrollo de React
-      //origin: "http://localhost:8081", // Permitir el frontend en desarrollo de React Native
+      // origin: "http://localhost:5173", // Permitir el frontend en desarrollo de React
+      origin: "http://localhost:8081", // Permitir el frontend en desarrollo de React Native
       credentials: true, // Permitir envío de cookies
     })
   );
